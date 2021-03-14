a11fcba564fa51f81e5f518f3b849978d19cfc34
Alex Sova <foxeyes.ml@gmail.com>
2021-03-14 16:34:44 +0300

    feat: shortcuts WIP

---
+++ examples/shortcut-create/create-concept.js
@@ -0,0 +1,39 @@
+// @ts-nocheck
+
+import { create } from '../../shortcuts/create.js';
+
+const template = /*html*/ `
+  <span css="el" set="textContent: text; onclick: clicked"></span>
+`;
+
+const css = {
+  el: {
+    color: '#f00',
+  },
+};
+
+const state = {
+  text: 'SOME TEXT',
+  clicked: () => {
+    console.log('CLICK');
+  },
+};
+
+create()
+  .tag('my-component')
+  .shadow(false) // default
+  .template(template)
+  .css(css)
+  .state(state)
+  .created
+  // constructor()
+  ()
+  .ready(() => {
+    // readyCallback
+  })
+  .connected
+  // connectedCAllback
+  ()
+  .disconnected
+  // disconnectedCallback
+  ();
