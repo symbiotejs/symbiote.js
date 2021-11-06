126c07bba0cffc860415af915958faca20737c22
Alex Sova <foxeyes.ml@gmail.com>
2021-11-06 10:22:06 +0300

    feat: UID pattern support

--- utils/UID.js
+++ utils/UID.js
@@ -1,10 +1,18 @@
+const CHARS = '1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm';
+const CHLENGTH = CHARS.length - 1;
+
 export class UID {
-  static generate() {
-    let allSymbolsStr = '1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm';
-    return 'XXXXXXXXX-XXX'.replace(/[X]/g, () => {
-      let rnd = Math.floor(Math.random() * (allSymbolsStr.length - 1));
-      let symbol = allSymbolsStr.substring(rnd, rnd + 1);
-      return symbol;
-    });
+  
+  /**
+   * 
+   * @param {String} [pattern] any symbols sequence with dashes. Default dash is used for human readability
+   * @returns {String} output example: v6xYaSk7C-kzZ
+   */
+  static generate(pattern = 'XXXXXXXXX-XXX') {
+    let uid = '';
+  	for (let i = 0; i < pattern.length; i++) {
+  		uid += pattern[i] === '-' ? pattern[i] : CHARS.charAt(Math.random() * CHLENGTH);
+  	}
+  	return uid;
   }
-}
+};
