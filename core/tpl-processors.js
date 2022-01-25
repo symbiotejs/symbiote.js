b7f0083288ab10203dcc10eaafa438c4290be8c3
Alex Sova <foxeyes.ml@gmail.com>
2022-01-25 17:59:56 +0300

    feat: text nodes data binding

--- core/tpl-processors.js
+++ core/tpl-processors.js
@@ -117,4 +117,53 @@ function domSetProcessor(fr, fnCtx) {
   });
 }
 
-export default [slotProcessor, refProcessor, domSetProcessor];
+const OPEN_TOKEN = '{{';
+const CLOSE_TOKEN = '}}';
+const SKIP_ATTR = 'skip-text';
+
+function getTextNodesWithTokens(el) {
+  let node;
+  let result = [];
+  let walk = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, {
+    acceptNode: (txt) => {
+      return !txt.parentElement?.hasAttribute(SKIP_ATTR) && txt.textContent.includes(OPEN_TOKEN) && txt.textContent.includes(CLOSE_TOKEN) && 1;
+    },
+  });
+  while ((node = walk.nextNode())) {
+    result.push(node);
+  }
+  return result;
+}
+
+/**
+ * @param {DocumentFragment} fr
+ * @param {any} fnCtx
+ */
+const txtNodesProcessor = function (fr, fnCtx) {
+  let txtNodes = getTextNodesWithTokens(fr);
+  txtNodes.forEach((/** @type {Text} */ txtNode) => {
+    let tokenNodes = [];
+    let offset;
+    // Splitting of the text node:
+    while (txtNode.textContent.includes(CLOSE_TOKEN)) {
+      if (txtNode.textContent.startsWith(OPEN_TOKEN)) {
+        offset = txtNode.textContent.indexOf(CLOSE_TOKEN) + 2;
+        txtNode.splitText(offset);
+        tokenNodes.push(txtNode);
+      } else {
+        offset = txtNode.textContent.indexOf(OPEN_TOKEN);
+        txtNode.splitText(offset);
+      }
+      // @ts-ignore
+      txtNode = txtNode.nextSibling;
+    }
+    tokenNodes.forEach((tNode) => {
+      let prop = tNode.textContent.replace(OPEN_TOKEN, '').replace(CLOSE_TOKEN, '');
+      fnCtx.sub(prop, (val) => {
+        tNode.textContent = val;
+      });
+    });
+  });
+};
+
+export default [slotProcessor, refProcessor, domSetProcessor, txtNodesProcessor];
