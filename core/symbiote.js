f9c63501a8652be6a44ebb9550a8820ae9033e71
Alex Sova <foxeyes.ml@gmail.com>
2022-07-06 12:14:19 +0300

    chore: export for the setNestedProp utility

--- core/symbiote.js
+++ core/symbiote.js
@@ -7,6 +7,7 @@ export { AppRouter } from './AppRouter.js';
 
 // Utils:
 export { UID } from '../utils/UID.js';
+export { setNestedProp } from '../utils/setNestedProp.js';
 export { applyStyles } from '../utils/dom-helpers.js';
 export { applyAttributes } from '../utils/dom-helpers.js';
 export { create } from '../utils/dom-helpers.js';
