2bd5edbc0673206604c008e779d095f77300c3d5
Aleksandr Grenishin <nd0ut.me@gmail.com>
2022-09-08 15:39:37 +0300

    feat: cache ctx name (#48)

--- core/BaseComponent.js
+++ core/BaseComponent.js
@@ -154,7 +154,14 @@ export class BaseComponent extends HTMLElement {
 
   /** @returns {String} */
   get ctxName() {
-    return this.getAttribute(DICT.CTX_NAME_ATTR)?.trim() || this.cssCtxName || this.autoCtxName;
+    let ctxName = this.getAttribute(DICT.CTX_NAME_ATTR)?.trim() || this.cssCtxName || this._cachedCtxName || this.autoCtxName;
+    /**
+     * Cache last ctx name to be able to access context when element becames disconnected
+     *
+     * @type {String}
+     */
+    this._cachedCtxName = ctxName;
+    return ctxName;
   }
 
   /** @returns {Data} */
